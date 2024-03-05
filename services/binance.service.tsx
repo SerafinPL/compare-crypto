53123f3d15c61110194fd96a58c774870e6c1074
serafinpl <ser.burberibar@gmail.com>
2024-03-05 18:08:42 -0200

    add files structure

--- services/binance.service.tsx
+++ services/binance.service.tsx
@@ -4,22 +4,21 @@ import axios from "axios";
 const baseApi: {
     domain: string;
     bookTickers: string;
-    
-  } = {
+} = {
     domain: 'https://api.binance.com/api/v3/',
     bookTickers: 'ticker/bookTicker',
-    
 }
 
 
 export const coinList: string[] = ['BTC', 'ETH', 'PAXG', 'BCH', 'SOL',
     'LTC', 'AVAX', 'ETC', 'INJ', 'TIA',
     'ATOM', 'FIL', 'NEAR', 'OSMO', 'XNO',
-    'KAVA', 'STORJ', '1INCH', 'XRP', 'ONT', 'ALGO', 'ASTR', 'DAR', 'TRX', 'DOGE'];
+    'KAVA', 'STORJ', '1INCH', 'XRP', 
+    'ONT', 'ALGO', 'ASTR', 'DAR', 'TRX', 'DOGE'];
 
 export const stringListT: string[] = coinList.map(el => el + 'USDT');
 
-export const getTradeInfo:() => any = () => {
+export const getTradeInfo: () => any = () => {
     let paramsToSend: string = ``;
 
     stringListT.forEach((param: string, index: number) => {
