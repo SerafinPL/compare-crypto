a51c15549e657312e10a4799789316aa0e71dd39
serafinpl <ser.burberibar@gmail.com>
2024-03-05 20:07:32 -0200

    routing path

--- components/NaviBar.tsx
+++ components/NaviBar.tsx
@@ -1,19 +1,23 @@
 'use client'
 import { useState } from "react";
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const NaviBar: React.FunctionComponent = () => {
 
+    const pathname = usePathname();
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const menuArray = ['Dashboard', 'Coin', 'Compare',];
 
-    const menuView = menuArray.map(el => <li key={`menuItem${el.toLocaleLowerCase()}`}><Link href={`/${el.toLocaleLowerCase()}`}>{el}</Link></li>)
+    const menuView = menuArray.map(el => <li key={`menuItem${el.toLocaleLowerCase()}`}>
+        <Link href={`/${el.toLocaleLowerCase()}`} className={`link ${pathname === el.toLocaleLowerCase() ? 'active' : ''}`}>{el}</Link>
+    </li>)
 
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
-                <div className={`dropdown ${dropdownOpen && 'dropdown-open'}`}>
+                <div className={`dropdown ${dropdownOpen ? 'dropdown-open' : ''}`}>
                     <div className="btn btn-ghost lg:hidden" onClick={() => setDropdownOpen(prev => !prev)}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
