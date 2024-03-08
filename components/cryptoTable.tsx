ca7b696ca0abf6ae9de6490ca358f7b981f16c2e
serafinpl <ser.burberibar@gmail.com>
2024-03-08 18:48:02 -0200

    icons to consts

--- components/cryptoTable.tsx
+++ components/cryptoTable.tsx
@@ -7,46 +7,51 @@ import { SymbolContext } from "@/context/symbolsContext";
 const CryptoTable: React.FunctionComponent = () => {
 
     let context = useContext(SymbolContext);
-
     const loadingValue: number = context?.loading || 0;
-
     const innerSymbolList = context?.symbolList || {};
+
+    //** VIEW VARS**/
+
+    const okIco = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+    </svg>;
+
+    const nonIco = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+    </svg>;
+
     const tableView = Object.keys(innerSymbolList).map((symbol, index) => {
 
         return (
             <tr key={`cryptoRow${symbol}`}>
                 <th>{++index}</th>
                 <td>{symbol}</td>
-                <td>{innerSymbolList[symbol].binance ?
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                    </svg> :
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-                }</td>
-                <td>{innerSymbolList[symbol].coinbase ?
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                    </svg> :
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-                }</td>
-                <td>{innerSymbolList[symbol].gateio ?
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                    </svg> :
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-                }</td>
+                <td>
+                    {innerSymbolList[symbol].binance ? okIco : nonIco}
+                </td>
+                <td>
+                    {innerSymbolList[symbol].coinbase ? okIco : nonIco}
+                </td>
+                <td>
+                    {innerSymbolList[symbol].gateio ? okIco : nonIco}
+                </td>
             </tr>
         )
     });
+
     return (
         <div className="min-w-full min-h-screen	">
-            <div className="radial-progress bg-primary text-primary-content border-4 border-primary" style={{ "--value": loadingValue }} role="progressbar">{context?.loading}%</div>
+
+            <div className="flex min-w-full p-3">
+                <div className="flex-none p-3">
+                    {context?.loading}%
+                </div>
+                <div className="flex-auto w-10/12 p-3">
+                    <progress className="progress progress-secondary " value={loadingValue} max="100"></progress>
+                </div>
+
+            </div>
+
             <div className="overflow-x-auto">
                 <table className="table table-xs">
                     <thead>
