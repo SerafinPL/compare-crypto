5e308a48dbdf01d154765359c75e937dbdcbc0ce
serafinpl <ser.burberibar@gmail.com>
2024-03-08 18:30:01 -0200

    add gateIo service add files

---
+++ services/gateio.service.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { listKey } from "@/context/symbolsContext";
+
+const baseApi: {
+    domain: string;
+    info: string;
+} = {
+    domain: 'https://api.gateio.ws/api/v4/',
+    info: 'flash_swap/currency_pairs',
+}
+
+export const getSymbolsFromGateIo: () => any = () => {
+    return axios.get(`${baseApi.domain}${baseApi.info}`).then(res => setGateIoSymbols(res.data), err => console.log);
+}
+
+const setGateIoSymbols: (data: { buy_currency: string, sell_currency: string }[]) => { listBase: listKey } = (data) => {
+
+    const listBase: listKey = {};
+    const listQuote: listKey = {};
+
+    data.forEach(rec => {
+        const keyBase: string = rec.buy_currency;
+        const keyQuote: string = rec.sell_currency;
+        listQuote[keyQuote] = true;
+        listBase[keyBase] = true;
+    })
+    return { listBase, listQuote }
+}
+
