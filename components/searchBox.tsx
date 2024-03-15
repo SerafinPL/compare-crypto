bdf03cf7ae23699a906b179088e08ffe2dbd8f42
serafinpl <ser.burberibar@gmail.com>
2024-03-15 20:53:39 -0200

    working with dropdown forec open button

--- components/searchBox.tsx
+++ components/searchBox.tsx
@@ -7,7 +7,10 @@ import { SymbolContext } from "@/context/symbolsContext";
 
 type searchProps = {
     input: string;
-    setInput: Dispatch<SetStateAction<string>>
+
+    setInput: Dispatch<SetStateAction<string>>;
+    dropDownOpen: string;
+    setDropDownState: Dispatch<SetStateAction<boolean>>;
 };
 
 const SearchBox = ({ input, setInput }: searchProps) => {
@@ -16,13 +19,17 @@ const SearchBox = ({ input, setInput }: searchProps) => {
         setInput(eve.target.value)
     }
 
-    return (<div>
+    return (<div className="flex items-center gap-0">
+        <button className="btn btn-square btn-outline">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+            </svg>
+        </button>
         <label className="input input-bordered flex items-center gap-2">
             <input type="text" className="grow" placeholder="Search base coin" value={input} onChange={onChangeHandler} />
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
-
         </label>
-    
+        
     </div>
     );
 }
--- components/selectSymbol.tsx
+++ components/selectSymbol.tsx
@@ -1,27 +1,29 @@
 'use client';
-import { useContext, useState,Dispatch,SetStateAction } from "react";
+import { useContext, useState, Dispatch, SetStateAction } from "react";
 
 import { SymbolContext } from "@/context/symbolsContext";
 import SearchBox from "./searchBox";
 
 type selectProps = {
-    baseCoin:string;
+    baseCoin: string;
     setBaseCoin: Dispatch<SetStateAction<string>>;
-  };
+};
 
-const SelectSymbol = ({baseCoin,setBaseCoin}:selectProps) => {
+const SelectSymbol = ({ baseCoin, setBaseCoin }: selectProps) => {
 
     const [searchInput, setSearchInput] = useState<string>('');
+    const [dropDownOpen, setDropDownState] = useState<boolean>(false);
 
     let context = useContext(SymbolContext);
     const innerSymbolList = context?.symbolList || {};
     const noEmpty = searchInput.trim().length > 0;
 
-    const clickHandler = (symbol:string) => {
-        setBaseCoin(symbol)
-        setSearchInput('')
+    const clickHandler = (symbol: string) => {
+        setBaseCoin(symbol);
+        setSearchInput('');
+        setDropDownState(false);
     }
-   
+
 
     const optionsRec = Object.keys(innerSymbolList).map(symbol => {
         if (symbol.search(searchInput.trim().toUpperCase()) > -1 && noEmpty) {
@@ -30,19 +32,17 @@ const SelectSymbol = ({baseCoin,setBaseCoin}:selectProps) => {
     });
 
     return (
-        
-    
-    <div className="float-right">
-        <div className={`w-40 float-right flex items-center`}>
-        {baseCoin}
-        </div>
-        <div className={`w-80 dropdown ${noEmpty ? 'dropdown-open' : ''}`}>
-            <SearchBox input={searchInput} setInput={setSearchInput} />
-            <ul className={`${noEmpty ? '' : 'hidden'} dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-80`}>
-                {optionsRec}
-            </ul>
-        </div>
-    </div>);
+        <div className="float-right flex justify-center items-center">
+            <div className={`w-40 flex justify-center items-center`}>
+                {baseCoin}
+            </div>
+            <div className={`w-96 dropdown ${noEmpty ? 'dropdown-open' : ''}`}>
+                <SearchBox input={searchInput} setInput={setSearchInput} dropDownOpen={dropDownOpen} setDropDownState={setDropDownState} />
+                <ul className={`${noEmpty ? '' : 'hidden'} dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-80`}>
+                    {optionsRec}
+                </ul>
+            </div>
+        </div>);
 }
 
 export default SelectSymbol
