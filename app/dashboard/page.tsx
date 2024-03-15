512378197094546e370b35f7e773af0f4d0a26d6
serafinpl <ser.burberibar@gmail.com>
2024-03-15 09:00:58 -0200

    searchboX finish

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,16 @@
+'use client';
+import { useContext, useState,Dispatch,SetStateAction } from "react";
+
 import CryptoTable from "@/components/cryptoTable";
 import SelectSymbol from "@/components/selectSymbol";
 
-const Dashboard: React.FunctionComponent = () => {
+const Dashboard = () => {
+
+  const [baseCoin, setBaseCoin] = useState<string>('USD');
+
   return (
     <div>
-      <SelectSymbol/>
+      <SelectSymbol baseCoin={baseCoin} setBaseCoin={setBaseCoin}/>
       <CryptoTable />
     </div>
   );
--- components/selectSymbol.tsx
+++ components/selectSymbol.tsx
@@ -1,27 +1,44 @@
 'use client';
-import { useContext, useState } from "react";
+import { useContext, useState,Dispatch,SetStateAction } from "react";
 
 import { SymbolContext } from "@/context/symbolsContext";
 import SearchBox from "./searchBox";
 
-const SelectSymbol: React.FunctionComponent = () => {
+type selectProps = {
+    baseCoin:string;
+    setBaseCoin: Dispatch<SetStateAction<string>>;
+  };
+
+const SelectSymbol = ({baseCoin,setBaseCoin}:selectProps) => {
 
     const [searchInput, setSearchInput] = useState<string>('');
 
     let context = useContext(SymbolContext);
     const innerSymbolList = context?.symbolList || {};
-
     const noEmpty = searchInput.trim().length > 0;
+
+    const clickHandler = (symbol:string) => {
+        setBaseCoin(symbol)
+        setSearchInput('')
+    }
+   
+
     const optionsRec = Object.keys(innerSymbolList).map(symbol => {
         if (symbol.search(searchInput.trim().toUpperCase()) > -1 && noEmpty) {
-            return (<li className="cursor-pointer" key={`cryptoSymbolSearchRow${symbol}`}>{symbol}</li>)
+            return (<li className="cursor-pointer" key={`cryptoSymbolSearchRow${symbol}`} onClick={() => clickHandler(symbol)}>{symbol}</li>)
         }
     });
 
-    return (<div className="float-right">
-        <div className={`w-80`}>
+    return (
+        
+    
+    <div className="float-right">
+        <div className={`w-40 float-right flex items-center`}>
+        {baseCoin}
+        </div>
+        <div className={`w-80 dropdown ${noEmpty ? 'dropdown-open' : ''}`}>
             <SearchBox input={searchInput} setInput={setSearchInput} />
-            <ul className={`${noEmpty ? 'block' : 'hidden'} dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-80`}>
+            <ul className={`${noEmpty ? '' : 'hidden'} dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-80`}>
                 {optionsRec}
             </ul>
         </div>
